a041d5fde49d11861f7960c173fa641647dc7da2
Omar Jbara <omar.jbara2@gmail.com>
2016-04-14 06:14:56 +0200

    Added student profiles

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -36,40 +36,4 @@ class AuthController extends Controller
 
 
 
-
-
-    /**
-     * @Route("/register", name="register")
-     * @Method({"POST", "GET"})
-     */
-    public function registerAction(Request $request)
-    {
-        $user = new User();
-//        $form = $this->createForm(UserType::class, $user);
-//        $form->handleRequest($request);
-
-        if($request->getMethod() == 'POST'){
-            $user->setUsername($request->get('username'));
-            $user->setEmail($request->get('email'));
-            $user->setPlainPassword($request->get('plainPassword'));
-            $pass = $this->get('security.password_encoder')->encodePassword($user, $user->getPlainPassword());
-            $user->setPassword($pass);
-            $user->setCreatedAt(new \DateTime());
-            $user->addRole($this->getDoctrine()->getRepository(Role::class)->findOneBy(['role' => 'ROLE_USER']));
-            $em = $this->getDoctrine()->getManager();
-            if($em->getRepository(User::class)->findOneBy(['username' => $user->getUsername()]) == null || $em->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]) == null){
-                $em->persist($user);
-                $em->flush();
-                return $this->redirectToRoute('homepage');
-
-            } else {
-                return $this->render('auth/register.html.twig', ['form' => $form->createView(), 'error' => "L'utilisateur ou l'adresse email est déjà utilisé !"]);
-
-            }
-
-        } else return $this->render('auth/register.html.twig');
-
-
-
-    }
 }
