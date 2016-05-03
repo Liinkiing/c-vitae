3cf264d87cd3076b1362c3a353a0e39bb75b3049
Omar Jbara <omar.jbara2@gmail.com>
2016-05-03 23:29:09 +0200

    Added even more search fields

--- src/AppBundle/Repository/StudentRepository.php
+++ src/AppBundle/Repository/StudentRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AppBundle\Repository;
+
 use AppBundle\Entity\Student;
 
 /**
@@ -26,7 +27,8 @@ class StudentRepository extends \Doctrine\ORM\EntityRepository
 
     }
 
-    public function findByTPGroup($tp){
+    public function findByTPGroup($tp)
+    {
         $qb = $this->getEntityManager()->createQueryBuilder();
         return $qb->select('student')
             ->from('AppBundle:Student', 'student')
@@ -36,7 +38,8 @@ class StudentRepository extends \Doctrine\ORM\EntityRepository
             ->getResult();
     }
 
-    public function findByFirstnameOrLastname($name){
+    public function findByFirstnameOrLastname($name)
+    {
         $qb = $this->getEntityManager()->createQueryBuilder();
         return $qb->select('student')
             ->from('AppBundle:Student', 'student')
@@ -47,46 +50,91 @@ class StudentRepository extends \Doctrine\ORM\EntityRepository
             ->getResult();
     }
 
-    public function findWithParams($group = ['A', 'B', 'C', 'D'], $name = null, $age = null, $role = null, $bac = null, $order = 'ASC'){
+    public function findWithParams($group = ['A', 'B', 'C', 'D'], $name = null, $age = null, $role = null, $bac = null, $programmingLanguages = null, $order = 'ASC')
+    {
 
         $qb = $this->getEntityManager()->createQueryBuilder();
         $result = $qb->select('student')
             ->from('AppBundle:Student', 'student')
             ->where($qb->expr()->in('student.groupeTp', '?1'))->setParameter(1, $group);
-        if($name != null){
+        if ($name != null) {
             $result->andWhere($qb->expr()->like(
                 "CONCAT(student.firstName, ' ', student.lastName)",
                 "?2"
             ))->setParameter(2, "%$name%");
         }
-        if($age != null){
+        if ($age != null) {
             $result->andWhere($qb->expr()->eq('student.age', '?3'))->setParameter(3, $age);
         }
-        if($role != null){
+        if ($role != null) {
             $result->andWhere($qb->expr()->in('student.projectRole', '?4'))->setParameter(4, $role);
         }
-        if($bac != null){
+        if ($bac != null) {
             $result->andWhere($qb->expr()->in('student.bac', '?5'))->setParameter(5, $bac);
+        }
+        if ($programmingLanguages != null) {
+            for ($i = 0; $i < count($programmingLanguages); $i++) {
+                $parameterCount = $i + 6;
+                $result->andWhere($qb->expr()->like('student.programmingLanguages', "?$parameterCount"))->setParameter($parameterCount, "%$programmingLanguages[$i]%");
+            }
         }
         $result->orderBy('student.lastName', $order);
         return $result->getQuery()->getResult();
 
     }
 
-    public function findProjectRoles(){
+    public function findProjectRoles()
+    {
         $qb = $this->getEntityManager()->createQueryBuilder();
         $roles = $qb->select('student.projectRole')->distinct()
             ->from('AppBundle:Student', 'student')
             ->getQuery()
             ->getResult();
-        $final =[];
-        foreach($roles as $role){
+        $final = [];
+        foreach ($roles as $role) {
             array_push($final, $role['projectRole']);
         }
         return $final;
     }
 
-    public function findByGroups(){
+    /**
+     * @return array
+     */
+    public function findProgrammingLanguages(){
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $langs = $qb->select('student.programmingLanguages')->distinct()
+            ->from('AppBundle:Student', 'student')
+            ->getQuery()
+            ->getScalarResult();
+        $temp = [];
+        foreach($langs as $lang){
+            array_push($temp, $lang['programmingLanguages']);
+        }
+        $langs = [];
+        foreach($temp as $item){
+            foreach(explode(',', $item) as $language){
+                if($language) array_push($langs, trim($language));
+            }
+        }
+        return array_unique($langs);
+    }
+
+    public function findSecteurVise()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $secteurs = $qb->select('student.secteur')->distinct()
+            ->from('AppBundle:Student', 'student')
+            ->getQuery()
+            ->getResult();
+        $final = [];
+        foreach ($secteurs as $secteur) {
+            array_push($final, $secteur['secteur']);
+        }
+        return $final;
+    }
+
+    public function findByGroups()
+    {
         $qb = $this->getEntityManager()->createQueryBuilder();
         return $qb->select('student')
             ->from('AppBundle:Student', 'student')
